79164a4251e24326704e3c7388986a201bb5b9d2
Otarion <c.garsault@laposte.net>
2024-03-29 15:24:33 +0100

    first

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,11 @@
+            <x-layout>
+            <div class="space-y-10 md:space-y-16">
+
+                @foreach($posts as $post)
+
+                <x-post :$post list />
+               
+                @endforeach
+                {{ $posts ->links() }}
+            </div>
+        </x-layout>
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,7 @@
+<x-layout :title="$post->title">
+    <div class="space-y-10 md:space-y-16">
+                    
+        <x-post :$post />
+
+    </div>
+</x-layout>
